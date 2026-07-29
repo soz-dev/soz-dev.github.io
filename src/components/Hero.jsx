9240d204e8b12e595e708cfae3dbc11078310992
Sofyan Zarouri <sofyan.zarouri@laposte.fr>
2026-07-29 12:02:26 +0200

    Remove scroll indicator from Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -122,20 +122,6 @@ export default function Hero() {
         </motion.div>
       </div>
 
-      {/* Scroll indicator */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.8 }}
-        className="absolute bottom-10 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2"
-      >
-        <span className="text-xs font-mono text-slate-700 tracking-[0.3em] uppercase">scroll</span>
-        <motion.div
-          animate={{ scaleY: [1, 0.5, 1], opacity: [0.3, 0.7, 0.3] }}
-          transition={{ repeat: Infinity, duration: 2 }}
-          className="w-px h-10 bg-gradient-to-b from-purple-500 to-transparent"
-        />
-      </motion.div>
     </section>
   )
 }
